2e8b8600fe12df3a43b2c147b84c913679482bfe
Пацьора Дмитро <workdmytropat@gmail.com>
2026-01-19 11:51:29 +0200

    feat(config): add configuration class for agent
    Add a new Configuration class using Pydantic to manage agent settings,
    including model names, retries, and script limits(Without this constraint, agent will likely generate a 3-minute long script for a video that should only be 15 seconds). Supports loading from
    environment variables or RunnableConfig.

---
+++ backend/src/configuration.py
@@ -0,0 +1,47 @@
+import os
+from pydantic import BaseModel, Field
+from typing import Any, Optional
+
+from langchain_core.runnables import RunnableConfig
+
+class Configuration(BaseModel):
+    """The configuration for the agent."""
+
+    writer_model: str = Field(
+        default="gemini-2.0-flash",
+        description = "The name of the language model to use for the agent's query generation.",
+    )
+
+    visual_model: str = Field(
+        default="gemini-2.0-flash-exp",
+        description="The 'Nano-Banana' model used for creating a video content" 
+    )
+
+    max_retries: int = Field(
+        default=3,
+        description="How many retries need to perform"
+    )
+
+    script_max_words: int = Field(
+        default=100,
+        description="Limit script length to keep video short."
+    )
+
+    @classmethod
+    def from_runnable_config(
+        cls, config: Optional[RunnableConfig] = None
+    ) -> "Configuration":
+        """Create a Configuration instance from a RunnableConfig."""
+
+        configurable = (
+            config['configurable'] if config and "configurable" in config else {}
+        )
+
+        raw_values: dict[str, Any] = {
+            name: os.environ.get(name.upper() , configurable.get(name))
+            for name in cls.model_fields.keys()
+        }
+
+        values = {k: v for k,v in raw_values.items() if v is not None}
+
+        return cls(**values)
